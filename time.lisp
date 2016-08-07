dbe93600932babfbd93ff038e9dbac336847fb75
Quinn Evans <qgevans@gmail.com>
2016-08-07 15:34:28 -0600

    Fix bugs, other stupidity

--- time.lisp
+++ time.lisp
@@ -4,14 +4,14 @@
   (declare (type (or stream boolean) stream))
   (multiple-value-bind (seconds minutes hours day month year day-of-week dst tz) (get-decoded-time)
       (format stream "~[Monday~;Tuesday~;Wednesday~;Thursday~;Friday~;Saturday~;Sunday~] ~
-the ~A~[th~;st~;nd~;rd~:;th~] ~
+the ~D~[th~;st~;nd~;rd~:;th~] ~
 of ~[World's End~;January~;February~;March~;April~;May~;June~;July~;August~;September~;October~;November~;December~] ~
-~A ~
-~A:~A:~A ~
-UTC~C~A~@[ (DST active)~*~]~%"
+~D ~
+~2,'0D:~2,'0D:~2,'0D ~
+UTC~@D~@[ (DST active)~*~]"
 	      day-of-week
 	      day (second (multiple-value-list (truncate day 10)))
 	      month
 	      year
 	      hours minutes seconds
-	      (if (< tz 0) #\+ #\-) tz dst)))
+	      (- tz) dst)))
